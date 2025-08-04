0bf7843bb56cc30c04f5e9d1a442af437018fc4d
NavdeepGunupuram <gunupuramnavdeep@gmail.com>
2025-08-04 10:44:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <BrowserRouter basename="/ecommerce">
+    <BrowserRouter basename="/S219_JenkinsPractice">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
