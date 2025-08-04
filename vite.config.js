0ff22f699a660e40fbfd2d01a9a81b6300b1b2d0
NavdeepGunupuram <gunupuramnavdeep@gmail.com>
2025-08-04 10:48:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   base: '/S219_JenkinsPractice/',
+  plugins: [react()]
 })
+
