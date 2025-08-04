19974c8636e1bfd20190498ce5cca4de0deae9c8
NavdeepGunupuram <gunupuramnavdeep@gmail.com>
2025-08-04 10:49:42 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { CartProvider } from "./context/CartContext";
+import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <CartProvider>
-  <App/>
-</CartProvider>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <App />
+  </BrowserRouter>
 )
